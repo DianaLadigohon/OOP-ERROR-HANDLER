91d97ea835b53f0ccbec399d62ad7b1b29f19a46
kian <148313755+itsmekaleb@users.noreply.github.com>
2023-10-24 12:49:19 +0800

    05.NullPointersException.java

--- 05.NullPointersException.java
+++ 05.NullPointersException.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Reyna{
+public class aretalla{
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         while(true){
