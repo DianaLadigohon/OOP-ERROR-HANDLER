de2517181b106fc5cd2bc32ab2d604388b7d7fb1
kian <148313755+itsmekaleb@users.noreply.github.com>
2023-10-24 12:43:11 +0800

    02.ClassNotFoundException.java

--- 02.ClassNotFoundException.java
+++ 02.ClassNotFoundException.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Lee {
+public class aretalla {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         while(true){
