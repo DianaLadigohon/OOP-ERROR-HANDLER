76823a538a8f606166224d49a51856606fcbe884
DianaLadigohon <148313062+DianaLadigohon@users.noreply.github.com>
2023-10-24 13:19:34 +0800

    Update 02.ClassNotFoundException.java

--- 02.ClassNotFoundException.java
+++ 02.ClassNotFoundException.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class aretalla {
+public class diana {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         while(true){
